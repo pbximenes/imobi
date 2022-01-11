908470928e7090964ab6907fac7c73186d71c341
Ximenes <paulo.ximenes@effem.com>
2022-01-11 15:51:30 -0300

    terceiro commit

--- autenticacao/views.py
+++ autenticacao/views.py
@@ -1,3 +1,5 @@
+from django.http import request
 from django.shortcuts import render
-
-# Create your views here.
+from django.http import HttpResponse
+def cadastro(request):
+    return HttpResponse('TESTE')
