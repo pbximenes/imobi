00d3890effed42e98bc64d627cb3b250fb08c2bc
Ximenes <paulo.ximenes@effem.com>
2022-01-12 13:49:14 -0300

    proxima fase

--- imobi/settings.py
+++ imobi/settings.py
@@ -118,7 +118,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'templates/static'),)
+STATICFILES_DIRS = (os.path.join(BASE_DIR, os.path.join('templates', 'static')),)
 STATIC_ROOT = os.path.join('static')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
